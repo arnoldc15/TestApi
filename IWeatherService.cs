a84307399c9bb00b1da73846135da30123e5fd30
Admin <Admin@ARNOLDPC>
2020-02-29 23:14:56 +1100

    First Commit

---
+++ IWeatherService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi
+{
+    public interface IWeatherService
+    {
+        IEnumerable<WeatherForecast> GetAllWeatherForecasts();
+    }
+
+}
